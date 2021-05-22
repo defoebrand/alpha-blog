0f82bdd2ee8b62d87f696986b0c32d6e11a2cb62
Brandon Defoe <defoe.brand@gmail.com>
2021-05-22 21:39:32 +0300

    Add create Article action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,28 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :description)
   end
 end
