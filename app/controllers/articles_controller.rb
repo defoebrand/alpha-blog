3e5bcea8139fa975b8b87d8b4c249fe94d421d23
Brandon Defoe <defoe.brand@gmail.com>
2021-05-22 22:14:09 +0300

    Add edit and update article actions + clean up controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,39 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update]
+  before_action :new_article, only: %i[new create]
+  around_action :save_article, only: %i[create update]
+
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
-  end
-
-  def create
-    @article = Article.new(articles_params)
-    if @article.save
-      flash[:notice] = 'Article was created successfully.'
-      redirect_to @article
-    else
-      render 'new'
-    end
-  end
+  def create; end
+  def update; end
 
   private
 
   def articles_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def new_article
+    @article = Article.new
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def save_article
+    flash_notice = params[:action] == 'create' ? 'created' : 'updated'
+    return_to_page = params[:action] == 'create' ? 'new' : 'edit'
+
+    if @article.update(articles_params)
+      flash[:notice] = "Article was #{flash_notice} successfully."
+      redirect_to @article
+    else
+      render return_to_page
+    end
   end
 end
