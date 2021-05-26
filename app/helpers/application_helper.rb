9de410600e39c6fb534dbdc25abec497c38a7703
Brandon Defoe <defoe.brand@gmail.com>
2021-05-26 17:24:11 +0300

    Add Users index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def avatar_img
+    content_tag(:div, image_tag('angry_squirrel.jpg', alt: 'avatar image', class: 'rounded mx-auto d-block img-fluid'), class: 'container avatar_icon justify-content-center p0')
+  end
 end
