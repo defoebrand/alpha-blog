b182b73267bfe93c5e257e58e22fe2d69cfeb583
Brandon Defoe <defoe.brand@gmail.com>
2021-05-28 02:49:37 +0300

    Add avatar image to article page + change update redirect & cond rend msg container

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def avatar_img
-    content_tag(:div, image_tag('angry_squirrel.jpg', alt: 'avatar image', class: 'rounded mx-auto d-block img-fluid'), class: 'container avatar_icon justify-content-center p0')
+  def avatar_img(size)
+    image_tag('angry_squirrel.jpg', alt: 'avatar image', class: 'rounded mx-auto d-block img-fluid', style: "max-width: #{size}px !important;")
   end
 end
