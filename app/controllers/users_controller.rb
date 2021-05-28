72f347062d2c115484f8113f530ebb3f5f6188fc
Brandon Defoe <defoe.brand@gmail.com>
2021-05-28 04:27:38 +0300

    Add authentication to blog app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,51 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  before_action :new_user, only: %i[new create]
+  around_action :save_user, only: %i[create update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
-  def new
+  def new; end
+
+  def edit; end
+
+  def update; end
+
+  def create; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def new_user
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def save_user
+    return_to_page = params[:action] == 'create' ? 'new' : 'edit'
 
-  def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "Your account information was successfully updated"
+      byebug
+      create_flash = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up!"
+      update_flash = "Your account information was successfully updated"
+      flash_notice = params[:action] == 'create' ? create_flash : update_flash
+      session[:user_id] = @user.id if params[:action] == 'create'
+      flash[:notice] = flash_notice
       redirect_to user_path(@user)
     else
-      render 'edit'
+      render return_to_page
     end
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up!"
-      redirect_to articles_path
-    else
-      render 'new'
-    end
-  end
-  private
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def avatar_img(size)
     image_tag('angry_squirrel.jpg', alt: 'avatar image', class: 'rounded mx-auto d-block img-fluid', style: "max-width: #{size}px !important;")
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
