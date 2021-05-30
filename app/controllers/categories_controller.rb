a28283ecb4a03863c7493762378a782780f25e5c
Brandon Defoe <defoe.brand@gmail.com>
2021-05-31 02:10:11 +0300

    Add admin check for tests and category creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :new_category, only: %i[new create]
+  before_action :require_admin, except: %i[index show]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -28,5 +29,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    unless (logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
